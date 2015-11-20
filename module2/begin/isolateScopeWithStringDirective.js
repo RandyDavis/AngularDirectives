9a3e7934f53340d02a5ae3b48af5e75c5e849ef3
Randy Davis <randy_davis@outlook.com>
2015-11-20 17:14:18 -0600

    finish @ local scope property example

---
+++ module2/begin/isolateScopeWithStringDirective.js
@@ -0,0 +1,13 @@
+(function () {
+
+    var app = angular.module('directivesModule');
+
+    app.directive('isolateScopeWithString', function () {
+        return {
+            scope: {
+              name: '@'
+            },
+            template: 'Name: {{name}}'
+        };
+    });
+}());
